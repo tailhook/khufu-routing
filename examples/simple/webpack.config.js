f7835863d13f7852cc53305882018a1594028188
Paul Colomiets <paul@colomiets.name>
2017-08-14 02:05:51 +0300

    Imports a skeleton for the example app

---
+++ examples/simple/webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    context: __dirname,
+    entry: "./index",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [{
+            test: /\.khufu$/,
+            loaders: ['babel-loader', 'khufu'],
+            exclude: /node_modules/,
+        }, {
+            test: /\.js$/,
+            loaders: ['babel-loader'],
+            exclude: /node_modules/,
+        }],
+    },
+    resolve: {
+        modules: ["/usr/lib/node_modules", "node_modules"],
+        mainFields: ["jsnext:main", "main"],
+    },
+    resolveLoader: {
+        modules: ["/usr/lib/node_modules"],
+        mainFields: ["webpackLoader", "jsnext:main", "main"],
+    },
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
 export class Router {
+    constructor(window, history) {
+        this._win = window
+        this._history = history
+        this._listeners = []
+        this._pop_state = this._pop_state.bind(this);
+        this.close = this.close.bind(this);
+        this._win.addEventListener("popstate", this._pop_state)
+    }
+    close() {
+        this._win.removeEventListener(this._pop_state)
+    }
+    _pop_state() {
+    }
+    getState() {
+        return {}
+    }
+    dispatch(ev) {
+
+    }
+    subscribe(callback) {
+        this._listeners.push(callback);
+        return function() {
+            let idx = this._listeners.indexOf(callback);
+            if(idx >= 0) {
+                this._listeners.splice(idx, 1);
+            }
+        }
+    }
 }
